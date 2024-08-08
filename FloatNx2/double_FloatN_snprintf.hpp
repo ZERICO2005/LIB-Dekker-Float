1e0d9297a37c9459f217695b944b1971f39ede01
ZERICO2005 <71151164+ZERICO2005@users.noreply.github.com>
2024-08-07 20:26:29 -0600

    Fixed typos

--- FloatNx2/double_FloatN_snprintf.hpp
+++ FloatNx2/double_FloatN_snprintf.hpp
@@ -1,6 +1,6 @@
 /*
-**	Author: zerico2005 (2023 - 2024)
-**	Project: ABS-Fractal-Explorer
+**	Author: zerico2005 (2024)
+**	Project: LIB-Dekker-Float
 **	License: MIT License
 **	A copy of the MIT License should be included with
 **	this project. If not, see https://opensource.org/license/MIT
